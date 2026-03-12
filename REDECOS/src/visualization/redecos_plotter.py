659dc6bc1aba5b1eb6592f091647ad3f1c20de54
pedro <pmontero@intecmar.gal>
2026-03-12 13:15:39 +0100

    Standardize headers for all Python files (OCXG API, CAPTA Project)

--- REDECOS/src/visualization/redecos_plotter.py
+++ REDECOS/src/visualization/redecos_plotter.py
@@ -1,8 +1,43 @@
-"""
-Plotting module for REDECOS (Coastal Observation Network) data.
-Author: Pedro Montero
-Organization: INTECMAR
-"""
+# -*- coding: utf-8 -*-
+
+#    !------------------------------------------------------------------------------
+#    !                       OCXG API, CAPTA Project
+#    !------------------------------------------------------------------------------
+#    !
+#    ! TITLE         : OCXG API_QUERY
+#    ! PROJECT       : CAPTA
+#    ! URL           : http://observatoriocosteiro.gal/es
+#    ! AFFILIATION   : INTECMAR
+#    ! DATE          : March 2026
+#    ! REVISION      : Montero 0.1
+#    !> @author
+#    !> Pedro Montero Vilar
+#    !
+#    ! DESCRIPTION:
+#    ! Preprocessing script for collecting data from OCXG API and processing them
+#    !--------------------------------------------------------------------------------------
+#
+#    MIT License
+#
+#    Copyright (c) 2026 INTECMAR
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
 
 import os
 import matplotlib.pyplot as plt
