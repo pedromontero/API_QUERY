70b76c6e2a0f0d410307569c6769a3794561e2a2
pedro <pmontero@intecmar.gal>
2026-03-12 21:53:41 +0100

    Upgrade all Python headers to Corporate Premium version with English descriptions and license blocks

--- REDECOS/src/data/redecos_exporter.py
+++ REDECOS/src/data/redecos_exporter.py
@@ -1,43 +1,56 @@
 # -*- coding: utf-8 -*-
 
-#    !------------------------------------------------------------------------------
-#    !                       OCXG API, CAPTA Project
-#    !------------------------------------------------------------------------------
-#    !
-#    ! TITLE         : OCXG API_QUERY
-#    ! PROJECT       : CAPTA
-#    ! URL           : http://observatoriocosteiro.gal/es
-#    ! AFFILIATION   : INTECMAR
-#    ! DATE          : March 2026
-#    ! REVISION      : Montero 0.1
-#    !> @author
-#    !> Pedro Montero Vilar
-#    !
-#    ! DESCRIPTION:
-#    ! Preprocessing script for collecting data from OCXG API and processing them
-#    !--------------------------------------------------------------------------------------
-#
-#    MIT License
-#
-#    Copyright (c) 2026 INTECMAR
-#
-#    Permission is hereby granted, free of charge, to any person obtaining a copy
-#    of this software and associated documentation files (the "Software"), to deal
-#    in the Software without restriction, including without limitation the rights
-#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#    copies of the Software, and to permit persons to whom the Software is
-#    furnished to do so, subject to the following conditions:
-#
-#    The above copyright notice and this permission notice shall be included in all
-#    copies or substantial portions of the Software.
-#
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#    SOFTWARE.
+"""
+!==============================================================================!
+!                OCXG API - CAPTA PROJECT (INTECMAR)                           !
+!==============================================================================!
+!                                                                              !
+! TITLE        : OCXG API_QUERY                                                !
+! PROJECT      : CAPTA (Coastal Observatory of Galicia)                        !
+! URL          : http://observatoriocosteiro.gal/es                            !
+! AFFILIATION  : INTECMAR (Xunta de Galicia)                                   !
+!                                                                              !
+! DATE         : March 2026                                                    !
+! VERSION      : 0.1.0-alpha                                                   !
+! REVISION     : Montero 0.1                                                   !
+!                                                                              !
+! AUTHOR       : Pedro Montero Vilar                                           !
+! CONTACT      : pmontero@intecmar.gal                                         !
+!                                                                              !
+! DESCRIPTION  : Handles exporting Redecos batches to structured Excel files.  !
+!                                                                              !
+!==============================================================================!
+!                               MIT LICENSE                                    !
+!------------------------------------------------------------------------------!
+! Copyright (c) 2026 INTECMAR                                                  !
+!                                                                              !
+! Permission is hereby granted, free of charge, to any person obtaining a copy  !
+! of this software and associated documentation files (the "Software"), to deal !
+! in the Software without restriction, including without limitation the rights  !
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    !
+! copies of the Software, and to permit persons to whom the Software is        !
+! furnished to do so, subject to the following conditions:                     !
+!                                                                              !
+! The above copyright notice and this permission notice shall be included in   !
+! all copies or substantial portions of the Software.                          !
+!                                                                              !
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   !
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      !
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  !
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       !
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, !
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    !
+! THE SOFTWARE.                                                                !
+!==============================================================================!
+"""
+
+__author__      = "Pedro Montero Vilar"
+__copyright__   = "Copyright 2026, INTECMAR"
+__license__     = "MIT"
+__version__     = "0.1.0"
+__maintainer__  = "Pedro Montero Vilar"
+__email__       = "pmontero@intecmar.gal"
+__status__      = "Development"
 
 import os
 import pandas as pd
